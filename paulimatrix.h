27038b2b5ace4c7164400dd5d1e1d476470bc1f9
Kevin Dungs <kevin@dun.gs>
2015-03-25 17:07:00 +0100

    Implement permutations and tests.

--- paulimatrix.h
+++ paulimatrix.h
@@ -5,6 +5,25 @@
 
 enum class PauliMatrix { X, Y, Z };
 
+auto isPermutation(PauliMatrix a, PauliMatrix b, PauliMatrix c) noexcept
+    -> bool {
+  return a != b && a != c && b != c;
+}
+
+auto isCyclic(PauliMatrix a, PauliMatrix b, PauliMatrix c) noexcept -> bool {
+  assert(isPermutation(a, b, c));
+  return (a == PauliMatrix::X && b == PauliMatrix::Y) ||
+         (b == PauliMatrix::X && c == PauliMatrix::Y) ||
+         (c == PauliMatrix::X && a == PauliMatrix::Y);
+}
+
+auto isAcyclic(PauliMatrix a, PauliMatrix b, PauliMatrix c) noexcept -> bool {
+  assert(isPermutation(a, b, c));
+  return (a == PauliMatrix::X && b == PauliMatrix::Z) ||
+         (b == PauliMatrix::X && c == PauliMatrix::Z) ||
+         (c == PauliMatrix::X && a == PauliMatrix::Z);
+}
+
 auto operator<<(std::ostream& os, PauliMatrix m) -> std::ostream & {
   switch (m) {
     case PauliMatrix::X:
--- test_paulimatrix.cc
+++ test_paulimatrix.cc
@@ -4,6 +4,31 @@
 #include <sstream>
 #include <string>
 
+auto test_isPermutation() -> void {
+  assert(isPermutation(PauliMatrix::X, PauliMatrix::Y, PauliMatrix::Z));
+  assert(!isPermutation(PauliMatrix::X, PauliMatrix::X, PauliMatrix::Z));
+}
+
+auto test_isCyclic() -> void {
+  assert(isCyclic(PauliMatrix::X, PauliMatrix::Y, PauliMatrix::Z));
+  assert(isCyclic(PauliMatrix::Y, PauliMatrix::Z, PauliMatrix::X));
+  assert(isCyclic(PauliMatrix::Z, PauliMatrix::X, PauliMatrix::Y));
+
+  assert(!isCyclic(PauliMatrix::X, PauliMatrix::Z, PauliMatrix::Y));
+  assert(!isCyclic(PauliMatrix::Y, PauliMatrix::X, PauliMatrix::Z));
+  assert(!isCyclic(PauliMatrix::Z, PauliMatrix::Y, PauliMatrix::X));
+}
+
+auto test_isAcyclic() -> void {
+  assert(isAcyclic(PauliMatrix::X, PauliMatrix::Z, PauliMatrix::Y));
+  assert(isAcyclic(PauliMatrix::Y, PauliMatrix::X, PauliMatrix::Z));
+  assert(isAcyclic(PauliMatrix::Z, PauliMatrix::Y, PauliMatrix::X));
+
+  assert(!isAcyclic(PauliMatrix::X, PauliMatrix::Y, PauliMatrix::Z));
+  assert(!isAcyclic(PauliMatrix::Y, PauliMatrix::Z, PauliMatrix::X));
+  assert(!isAcyclic(PauliMatrix::Z, PauliMatrix::X, PauliMatrix::Y));
+}
+
 auto test_ostreamOperator() -> void {
   auto ss = std::stringstream{};
   ss << PauliMatrix::X;
@@ -14,5 +39,8 @@ auto test_ostreamOperator() -> void {
 }
 
 int main() {
+  test_isPermutation();
+  test_isCyclic();
+  test_isAcyclic();
   test_ostreamOperator();
 }
