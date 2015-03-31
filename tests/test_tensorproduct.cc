99641d02a7cd638ac174968b4865e21cb9e57404
McUrbn <urban@aadr.de>
2015-03-31 21:02:59 +0200

    Added test for summation

--- tests/test_tensorproduct.cc
+++ tests/test_tensorproduct.cc
@@ -38,6 +38,25 @@ auto test_ostreamOperator() -> void {
   ss << TensorProduct<3>{PM::X, PM::Y, PM::Z};
   assert(ss.str() == std::string{"[X Y Z]"});
 }
+    
+auto test_summation() -> void {
+  int n = 4;
+  int sum = 0;
+  PM array[] = {PM::X,PM::Y,PM::Z};
+  for(int j = 0; j < 3; ++j)
+  {
+    for (int i1 = 0; i1 < 4; ++i1) {
+      for (int i2 = 0; i2 < 4; ++i2) {
+        auto t1 = TensorProduct<n>{};
+        auto t2 = TensorProduct<n>{};
+        t1.set<i1>(array[j]);
+        t2.set<i1>(array[j]);
+        sum += (t1*t2).trace();
+      }
+    }
+  }
+  assert(sum == 3*n/4);
+}
 
 int main() {
   test_equalityOperator();
@@ -46,4 +65,5 @@ int main() {
   test_set();
   test_trace();
   test_ostreamOperator();
+  test_summation();
 }
